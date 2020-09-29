4c63ded84b3f5a76b94d82a01db0084f1ad6e418
De4dSh0t <ruipsilva2001@hotmail.com>
2020-09-29 18:42:37 +0100

    Updated "ChatSystem" and Added "ChatBubble"

---
+++ Mini-Jogo1/Assets/Scripts/Chat/ChatBubble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChatBubble : MonoBehaviour
+{
+    private SpriteRenderer background;
+    private TMP_Text message;
+    
+    void Start()
+    {
+        background = transform.Find("Background").GetComponent<SpriteRenderer>();
+        message = transform.Find("Message").GetComponent<TMP_Text>();
+    }
+
+    /// <summary>
+    /// Chat bubble setup
+    /// </summary>
+    /// <param name="text"></param>
+    public void Setup(string text)
+    {
+        //Message
+        message.text = text;
+        
+        //Background
+        message.ForceMeshUpdate(); //Assures that the "GetRenderedValues" allways gets the latest values
+        Vector2 messageSize = message.GetRenderedValues(false);
+        Vector2 offset = new Vector2(2, 2);
+        background.size = messageSize + offset; //Resizes the text depending on message size
+    }
+}
--- Mini-Jogo1/Assets/Scripts/Chat/ChatSystem.cs
+++ Mini-Jogo1/Assets/Scripts/Chat/ChatSystem.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChatSystem : MonoBehaviour
 {
+    [SerializeField] private GameObject chatPanel;
+    [SerializeField] private GameObject bubblePrefab;
+    [SerializeField] private List<GameObject> messageList = new List<GameObject>();
+    [SerializeField] private int maxMessages = 20;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerTextInput.OnTextInput += DisplayText;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Removes the first message when it reaches the maximum
+        if (messageList.Count > maxMessages)
+        {
+            Destroy(messageList[0]);
+            messageList.Remove(messageList[0]);
+        }
+    }
+
+    private void DisplayText(string text)
+    {
+        //Implement text instantiation (with bubble background)
+        GameObject message = Instantiate(bubblePrefab, chatPanel.transform);
+        message.GetComponent<TMP_Text>().text = text;
+        messageList.Add(message);
     }
 }
