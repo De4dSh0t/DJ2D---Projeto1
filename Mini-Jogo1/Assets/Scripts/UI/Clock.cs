08e3d143f145ce8817aca3ccfd8a28bc9d52451f
De4dSh0t <ruipsilva2001@hotmail.com>
2020-10-06 16:45:17 +0100

    Added "Clock" script

---
+++ Mini-Jogo1/Assets/Scripts/UI/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private TMP_Text clockText;
+
+    void Start()
+    {
+        clockText = GetComponent<TMP_Text>();
+    }
+    
+    void Update()
+    {
+        clockText.text = DateTime.Now.ToShortTimeString();
+    }
+}
