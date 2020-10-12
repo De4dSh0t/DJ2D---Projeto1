a403fa5e4ccf652052526145de0f3c15eb69a2ca
De4dSh0t <ruipsilva2001@hotmail.com>
2020-10-12 19:10:12 +0100

    Added first "Virus" (Sorting Numbers)

---
+++ Mini-Jogo1/Assets/Scripts/Virus/Sort Numbers/ButtonBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    private TMP_Text txt;
+    private Button button;
+    private Color defaultColor;
+
+    public static Action<string> OnButtonPressed;
+    
+    void Start()
+    {
+        txt = GetComponentInChildren<TMP_Text>();
+        button = GetComponent<Button>();
+        defaultColor = button.image.color;
+
+        SortBehaviour.OnPlayerMiss += UnlockButton;
+        SortBehaviour.OnPlayerSuccess += Completed;
+    }
+
+    /// <summary>
+    /// Sets the number of the button
+    /// </summary>
+    public void SetNumber(string num)
+    {
+        txt.text = num;
+    }
+
+    /// <summary>
+    /// Locks the button when pressed
+    /// </summary>
+    public void LockButton()
+    {
+        button.enabled = false;
+        button.image.color = new Color(0.70f, 0.70f, 0.70f);
+        OnButtonPressed(txt.text);
+    }
+
+    /// <summary>
+    /// Unlocks the button when player fails to sort the numbers
+    /// </summary>
+    public void UnlockButton()
+    {
+        button.enabled = true;
+        button.image.color = defaultColor;
+    }
+
+    public void Completed()
+    {
+        button.image.color = new Color(0.43f, 0.92f, 0.31f);
+    }
+}
---
+++ Mini-Jogo1/Assets/Scripts/Virus/Sort Numbers/SortBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SortBehaviour : MonoBehaviour
+{
+    private readonly List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    private readonly int[] buttonNums = new int[10];
+    private int[] numArray; //Used in "CheckOrder"
+    private List<ButtonBehaviour> buttonsList = new List<ButtonBehaviour>();
+    private int index;
+
+    public static Action OnPlayerMiss;
+    public static Action OnPlayerSuccess;
+    
+    void Start()
+    {
+        numArray = numbers.ToArray();
+        buttonsList.AddRange(GetComponentsInChildren<ButtonBehaviour>());
+        ButtonBehaviour.OnButtonPressed += CheckOrder;
+        
+        PopulateArray(buttonNums, numbers);
+        SetButtonNum(buttonsList);
+    }
+
+    /// <summary>
+    /// Populates the array used to give each button a number
+    /// </summary>
+    /// <param name="bNums"></param>
+    /// <param name="tempList"></param>
+    private void PopulateArray(int[] bNums, List<int> tempList)
+    {
+        for (int i = 0; i < bNums.Length; i++)
+        {
+            int rIndex = Random.Range(0, tempList.Count - 1);
+            buttonNums[i] = tempList[rIndex];
+            tempList.RemoveAt(rIndex);
+        }
+    }
+
+    /// <summary>
+    /// Sets the number of each button from the "buttonNums" array
+    /// </summary>
+    /// <param name="bList"></param>
+    private void SetButtonNum(List<ButtonBehaviour> bList)
+    {
+        for (int i = 0; i < buttonNums.Length; i++)
+        {
+            bList[i].SetNumber(buttonNums[i].ToString());
+        }
+    }
+
+    /// <summary>
+    /// Checks if the player is sorting the number correctly
+    /// </summary>
+    /// <param name="num"></param>
+    private void CheckOrder(string num)
+    {
+        if (int.Parse(num) == numArray[index])
+        {
+            index++;
+
+            // Successfully finishes the mini challenge
+            if (index >= 10)
+            {
+                OnPlayerSuccess();
+            }
+        }
+        else
+        {
+            index = 0;
+            OnPlayerMiss();
+        }
+    }
+}
