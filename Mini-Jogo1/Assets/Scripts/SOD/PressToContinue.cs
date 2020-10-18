500cba75853e4f54ecd3a45be06658b5c929ecc9
De4dSh0t <ruipsilva2001@hotmail.com>
2020-10-18 20:49:43 +0100

    Created MainMenu and ScreenOfDeath Menu

---
+++ Mini-Jogo1/Assets/Scripts/SOD/PressToContinue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressToContinue : MonoBehaviour
+{
+    [SerializeField] private float timeToActivate;
+    [SerializeField] private float fadeRate;
+    private bool isVisible;
+    private TMP_Text text;
+    
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+    
+    void Update()
+    {
+        if (Time.time >= timeToActivate)
+        {
+            if (isVisible)
+            {
+                text.alpha -= fadeRate * Time.deltaTime;
+
+                if (text.alpha <= 0)
+                {
+                    isVisible = false;
+                }
+            }
+            else
+            {
+                text.alpha += fadeRate * Time.deltaTime;
+
+                if (text.alpha >= 1)
+                {
+                    isVisible = true;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
