e87e531b008715204009cf19a501d084cc2107a8
De4dSh0t <ruipsilva2001@hotmail.com>
2020-10-21 00:30:30 +0100

    Added watermark

---
+++ Mini-Jogo1/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField] private float time;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
